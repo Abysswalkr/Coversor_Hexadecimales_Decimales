8d6312aa7e76865d2b5f00c79704ee9ead29c0e4
Jose Gramajo <gra22907@uvg.edu.gt>
2023-02-22 12:18:44 -0600

    Lenguage changes

--- src/ConversorHexDec.java
+++ src/ConversorHexDec.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
 /**
- * Esta clase contiene un programa que convierte números de hexadecimal a decimal y viceversa.
+ * This class contains a program that converts numbers from hexadecimal to decimal and vice versa.
  */
 public class ConversorHexDec {
 
     /**
-     * Método principal que inicia el programa.
+     * Main method that starts the program.
      *
-     * @param args los argumentos de línea de comandos. No se utilizan en este programa.
+     * @param args command line arguments. They are not used in this program.
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrese un número hexadecimal de 3 dígitos:");
+        System.out.println("Enter a 3-digit hexadecimal number:");
         String hex = sc.nextLine();
 
-        // Convertir de hexadecimal a decimal
+        // Convert from hexadecimal to decimal
         int decimal = Integer.parseInt(hex, 16);
-        System.out.println("El número decimal equivalente es: " + decimal);
+        System.out.println("The hexadecimal number is: " + decimal);
 
-        System.out.println("Ingrese un número decimal de 3 dígitos:");
+        System.out.println("Enter a 3-digit decimal number:");
         int dec = sc.nextInt();
 
-        // Convertir de decimal a hexadecimal
+        // Convert from decimal to hexadecimal
         String hexa = Integer.toHexString(dec);
-        System.out.println("El número hexadecimal equivalente es: " + hexa.toUpperCase());
+        System.out.println("The equivalent hexadecimal number is: " + hexa.toUpperCase());
     }
 }
 
